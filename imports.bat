c6f882eb0c30302350e012a8d107d1aa245d4e48
zencyber <139611438+zencyber0@users.noreply.github.com>
2024-12-22 22:44:29 -0500

    Create imports.bat

---
+++ imports.bat
@@ -0,0 +1,8 @@
+echo Installing Requirements...
+pip install colorama
+pip install pystyle
+pip install webbrowser
+pip install requests
+echo We have downloaded all nessecary dependencies for Zen-Multi!
+pause 
+exit
